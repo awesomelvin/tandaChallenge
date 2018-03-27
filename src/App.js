f79d710fa585295655c8b05e9ed5226c469a4216
awesomelvin <awesomelvin8645@gmail.com>
2018-03-27 13:58:10 +0800

    Add some pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import PunctualityPage from './components/pages/PunctualityPage';
 
-const App = () => <div>hellow world</div>;
+const App = () => <div>
+  <Route path="/" exact component={HomePage} />
+  <Route path="/punctuality" exact component={PunctualityPage} />
+  </div>;
 
 export default App;
---
+++ src/components/pages/PunctualityPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PunctualityPage = () => (
+  <div>
+    Welcome to Punctuality Page!
+  </div>
+);
+
+export default PunctualityPage;
